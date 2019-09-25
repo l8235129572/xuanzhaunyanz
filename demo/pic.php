055279f80b06274a641e6a0fea7e76f0ff67874b
scupte121 <scupte1@163.com>
2019-09-25 21:51:04 +0800

    Update pic.php

--- demo/pic.php
+++ demo/pic.php
@@ -1,2 +1,11 @@
 <?php
 include_once '../extend/Yzt/Yzt.php';
+$Yzt = new \Yzt\Yzt('https://api.uomg.com/api/rand.img1', true, true, rand(20, 270));
+
+       // $info = $Yzt->move('static/upload/');
+        //return $info;
+        //die();
+        header("Content-type: image/png;text/html; charset=utf-8");
+       
+        imagepng($Yzt->changeCircularImg());
+        die();
